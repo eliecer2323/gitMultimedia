576c0c4c9062d0214dc310acbd5c10e5b4112824
DESKTOP-TPCCM3T\JorgeEliecerSierra <eliecer_2323@hotmail.com>
2018-05-26 17:40:35 -0500

    Se arreglan errores y se agrega la funcionalidad al guión 8

--- Scripts/Localidades.cs
+++ Scripts/Localidades.cs
@@ -7,11 +7,22 @@ public class Localidades : MonoBehaviour
 {
 
 	public AudioSource fuente;
+    public GameObject locAnterior;
+    public GameObject pergaminoActivo;
 
 	// Use this for initialization
 	void Start()
 	{
+        locAnterior = new GameObject();
+        locAnterior.AddComponent<Image>();
+        pergaminoActivo = null;
 	}
+
+    public void CerrarVentana()
+    {
+        pergaminoActivo.SetActive(false);
+        pergaminoActivo = null;
+    }
 
 	public void Reproducir()
 	{
@@ -20,25 +31,39 @@ public class Localidades : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (pergaminoActivo==null)
 		{
 			//Debug.Log("Pressed left click, casting ray.");
 			CastRay();
 		}
 	}
 
-	void CastRay()
+    void CastRay()
 	{
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 0);
 		if (hit)
 		{
-			Debug.Log(hit.collider.gameObject.name);
+			//Debug.Log(hit.collider.gameObject.name);
 			GameObject localidad = hit.collider.gameObject;
-			Image imgLocalidad = hit.collider.gameObject.GetComponent<Image>();
-			imgLocalidad.enabled = !imgLocalidad.enabled;
-			Reproducir();
-		}
-	}
+            if (locAnterior != localidad)
+            {
+                Image imgLocalidad = localidad.GetComponent<Image>();
+                //imgLocalidad.enabled = !imgLocalidad.enabled;
+                imgLocalidad.enabled = true;
+           
+                locAnterior.GetComponent<Image>().enabled = false;
+                Reproducir();
+            }
+            locAnterior = localidad;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject pergamino = localidad.transform.Find("pergamino").gameObject;
+                pergamino.SetActive(true);
+                pergaminoActivo = pergamino;
+            }
+        }
+    }
 
 }
